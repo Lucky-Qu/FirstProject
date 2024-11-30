8ae6e6c0d6138fe5e8500100e8a60eb2c2220e9d
LuckyQu <super.lucky.qu@gmail.com>
2024-11-30 23:13:00 +0800

    更新内容

--- service/Start.go
+++ service/Start.go
@@ -44,14 +44,32 @@ func StartService() {
 			"student": student,
 		})
 	})
-	g.DELETE("/FirstProject", func(c *gin.Context) {
+	g.DELETE("/FirstProject/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"msg": "删除成功",
 		})
 	})
 	g.PUT("/FirstProject", func(c *gin.Context) {
+		student := model.Student{}
+		num := c.Query("num")
+		if db.Model(&model.Student{}).Where("num = ?", num).First(&student); student.Name == "" {
+			panic("No")
+		}
+		if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+			err = v.RegisterValidation("AgeCheck", model.AgeCheck)
+			if err != nil {
+				panic(err)
+			}
+		}
+		student2 := model.Student{}
+		err = c.ShouldBindJSON(&student2)
+		if err != nil {
+			panic(err)
+		}
+		db.Model(&model.Student{}).Where(&student).Updates(&student2)
 		c.JSON(200, gin.H{
-			"msg": "更改成功",
+			"msg":     "更改成功",
+			"student": student2,
 		})
 	})
 	err = g.Run(config.URL)
